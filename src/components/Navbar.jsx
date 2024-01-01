1349e6052a22d786be460b9f103313d08df898a8
Ogoba David Edouye <114475043+DoyeDesigns@users.noreply.github.com>
2024-01-01 00:18:52 +0000

    edited theme change icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar() {
                   <input
                     type="checkbox"
                     value="synthwave"
-                    className="toggle theme-controller"
+                    className="toggle theme-controller bg-amber-300 border-sky-400 [--tglbg:theme(colors.sky.500)] checked:bg-blue-300 checked:border-blue-800 checked:[--tglbg:theme(colors.blue.900)] row-start-1 col-start-1 col-span-2"
                     onChange={toggleTheme}
                     checked={isChecked}
                   />
@@ -146,7 +146,7 @@ function Navbar() {
                   <input
                     type="checkbox"
                     value="synthwave"
-                    className="toggle theme-controller"
+                    className="toggle theme-controller bg-amber-300 border-sky-400 [--tglbg:theme(colors.sky.500)] checked:bg-blue-300 checked:border-blue-800 checked:[--tglbg:theme(colors.blue.900)] row-start-1 col-start-1 col-span-2"
                     onChange={toggleTheme}
                     checked={isChecked}
                   />
